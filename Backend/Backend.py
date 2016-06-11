7c3c24c6bc07672242aa0ad4a2123cac9c63f445
Naman Maheshwari <naman1901@gmail.com>
2016-06-12 00:45:23 +0530

    Backend done, hopefully

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -1,64 +1,123 @@
-from flask import Flask
-# from flask_oauthlib.client import OAuth
+from flask import Flask, render_template
 import twitter
-from havenondemand.hodclient import *
+from functions import *
+from statistics import mean
+import feedparser
 
 app = Flask(__name__)
 
 api = twitter.Api(
-    consumer_key='7jbKM37yIz9pPNAsVwktacerc',
-    consumer_secret='iEGbqG5fNHyJSsMLuJzib27lj7lI7CIiBtbC3YiT6Z17eU0XFe',
-    access_token_key='47313714-VnLZ0lFKLxQbewnxskwDB6Zbj4VnF0Fq6rbrjPqPI',
-    access_token_secret='HJuA4JBGOfFy7dZRVDQihw1l7dnkxVsNpOfer8YjZ0SZ9'
 )
 
-client = HODClient("921405ad-c2f6-48fb-b8b6-3e9044a5f716", version="v1")
+TWEET_COUNT = 3
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    # return 'Hi'
+    trends = api.GetTrendsCurrent()
+    tweets = []
+    temp = {}
+    hl_keys = set()
+    rss = feedparser.parse('http://rss.cnn.com/rss/edition_world.rss')
+    headlines = [item['title'] for item in rss['items']]
+    for headline in headlines[:3]:
+        for concept in concept_extract(headline):
+            hl_keys.add(concept)
+    for trend in trends[:2]:
+        trend = trend.name.encode('utf=8').decode('utf-8', 'ignore').replace('#', '')
+        trend = trend.strip()
+        print(trend)
+        tweets = api.GetSearch(term=trend, lang='en', count=TWEET_COUNT)
+        for tweet in tweets:
+            text = tweet.text.encode('utf=8').decode('ascii', 'ignore')
+            sentiment = sentiment_detect(tweet.text.encode('utf-8').decode('ascii', 'ignore'))
+            location = tweet.user.location
+            if not location or location == 'Global':
+                location = {
+                    'code': 'IN',
+                    'label': 'India'
+                }
+            else:
+                location = country_extract(location)
+            loc_code = location['code']
+            if loc_code not in list(temp.keys()):
+                temp[loc_code] = {}
+            if 'trends' not in list(temp[loc_code].keys()):
+                temp[loc_code]['trends'] = {}
+            temp[loc_code]['name'] = location['label']
+            if trend not in list(temp[loc_code]['trends'].keys()):
+                temp[loc_code]['trends'][trend] = []
+            temp[loc_code]['trends'][trend].append(sentiment)
 
-def sentiment_detect(sentence):
-    params = {'text': sentence}
-    response = client.get_request(params, HODApps.ANALYZE_SENTIMENT, async=False)
-    #print(response)
-    sentiment_value = response['aggregate']['score']
-    #print(sentiment_value)
-    return sentiment_value
+    i = 0
+    for hl_key in hl_keys:
+        i += 1
+        if i == 3:
+            break
+        tweets = api.GetSearch(term=hl_key, lang='en', count=TWEET_COUNT)
+        for tweet in tweets:
+            text = tweet.text.encode('utf=8').decode('ascii', 'ignore')
+            # print(text)
+            # print('HAHAHAHAHAHAHA')
+            sentiment = sentiment_detect(tweet.text.encode('utf-8').decode('ascii', 'ignore'))
+            location = tweet.user.location
+            if not location or location == 'Global':
+                location = {
+                    'code': 'IN',
+                    'label': 'India'
+                }
+            else:
+                location = country_extract(location)
+            loc_code = location['code']
+            if loc_code not in list(temp.keys()):
+                temp[loc_code] = {}
+            if 'trends' not in list(temp[loc_code].keys()):
+                temp[loc_code]['trends'] = {}
+            temp[loc_code]['name'] = location['label']
+            if trend not in list(temp[loc_code]['trends'].keys()):
+                temp[loc_code]['trends'][trend] = []
+            temp[loc_code]['trends'][trend].append(sentiment)
 
-def country_extract(location_data="India"):
-    params = {'text': location_data, 'entity_type': 'places_eng' }
 
+    map_output = []
+    for k1 in temp:
+        sentiments = []
+        for k2 in temp[k1]['trends']:
+            i += 1
+            temp[k1]['trends'][k2] = mean(temp[k1]['trends'][k2])
+            sentiments.append(temp[k1]['trends'][k2])
 
-    response = client.get_request(params, HODApps.ENTITY_EXTRACTION, async=False)
-    #print(response)
+        map_output.append({
+            'value': mean(sentiments),
+            'code': k1,
+            'name': temp[k1]['name']
+        })
 
-    if len(response['entities'])>0:
-        country_code = response['entities'][0]['additional_information']['place_country_code']
-        country_name = response['entities'][0]['original_text']
-        #print(country_code)
-        #print(country_name)
-
-    else:
-        country_code = "IN"
-        country_name = "India"
-
-    return country_code, country_name
+    return render_template('search.html', output={
+        'map': map_output,
+        'loc': temp
+    })
 
 
 @app.route('/search/<query>')
 def search_query(query):
-    query_string = ''
-    query_params = query.split('?')
+    # return query
+    query_params = query.split('_')
+    # print(query_params)
+    # return 'Hi'
     term = None
     since = None
     until = None
 
     for param in query_params:
         # Search with this keyword
-        param = param.split('=')
-        value = param[1]
-        param = param[0]
+        try:
+            param = param.split('=')
+            value = param[1]
+            param = param[0]
+        except:
+            continue
         if param == 'keyword':
             term = value
         # Time period start
@@ -68,8 +127,50 @@ def search_query(query):
         elif param == 'until':
             until = value
 
-        tweets = api.GetSearch(term=term, since=since, until=until, count=200)
+    map_output = []
+    loc_sentiments = {}
+    tweet_sentiment = []
+    tweets = api.GetSearch(term=term, since=since, until=until, count=200, lang='en')
+    for tweet in tweets:
+        sentiment = sentiment_detect(tweet.text.encode('utf-8').decode('ascii', 'ignore'))
+        location = tweet.user.location
+        if not location or location == 'Global':
+            location = {
+                'code': 'IN',
+                'label': 'India'
+            }
+        else:
+            location = country_extract(location)
+        if location['code'] in loc_sentiments:
+            loc_sentiments[location['code']].append(sentiment)
+        else:
+            loc_sentiments[location['code']] = [sentiment]
+        tweet_sentiment.append((tweet.text.encode('utf-8').decode('ascii', 'ignore'), sentiment))
+    tweet_list = []
+    for key in loc_sentiments:
+        loc_sentiments[key] = mean(loc_sentiments[key])
+        map_output.append({
+            'code': key,
+            'value': loc_sentiments[key],
+            'name': term
+        })
 
+    mu = mean([loc_sentiments[key] for key in loc_sentiments])
+    for i in range(len(tweet_sentiment)):
+        tweet_sentiment[i] = (tweet_sentiment[i][0], (tweet_sentiment[i][1]-mu)**2)
+    tweet_sentiment = sorted(tweet_sentiment, key=get_key)
+    if len(tweet_sentiment) > 5:
+        tweet_sentiment = tweet_sentiment[:5]
+
+    tweet_sentiment = [tweet for (tweet, s) in tweet_sentiment]
+
+    output = {
+        'tweets': tweet_sentiment,
+        'map_data': map_output
+    }
+
+    return render_template('search.html', output=output)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
---
+++ Backend/functions.py
@@ -0,0 +1,55 @@
+from havenondemand.hodclient import *
+
+client = HODClient("921405ad-c2f6-48fb-b8b6-3e9044a5f716", version="v1")
+
+
+def sentiment_detect(sentence):
+    print('1')
+    print(sentence)
+    print('2')
+    params = {'text': sentence}
+    response = client.get_request(params, HODApps.ANALYZE_SENTIMENT, async=False)
+    print(response)
+    try:
+        sentiment_value = response['aggregate']['score']
+    except:
+        sentiment_value = 0
+    print(sentiment_value)
+    return sentiment_value
+
+
+def country_extract(location_data):
+    params = {'text': location_data, 'entity_type': 'places_eng' }
+    response = client.get_request(params, HODApps.ENTITY_EXTRACTION, async=False)
+    location = {
+        'code': 'IN',
+        'label': 'India'
+    }
+    print(response)
+    try:
+        entity = response['entities'][-1]
+        loc_code = entity['additional_information']['place_country_code'].encode('utf-8').decode('ascii', 'ignore')
+        loc_label = entity['normalized_text'].encode('utf-8').decode('ascii', 'ignore')
+        location['code'] = loc_code
+        location['label'] = loc_label
+    except:
+        print('Loc error')
+    return location
+
+
+def concept_extract(headline):
+    print(headline)
+    params = {'text': headline}
+    response = client.get_request(params, HODApps.EXTRACT_CONCEPTS, async=False)
+    print(response)
+    concepts = []
+    try:
+        for entity in response['concepts']:
+            concepts.append(entity['concept'].encode('utf-8').decode('ascii', 'ignore'))
+    except:
+        pass
+    return concepts
+
+
+def get_key(item):
+    return item[1]
